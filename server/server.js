1cdd340565ac4fd3a64a2e149a310e8437b0d75d
Gagandeep Singh <gagan.ddun@gmail.com>
2020-07-23 21:47:45 +1000

    generate random tinyurl

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const randomstring = require('randomstring');
 
 const app = express();
 
-app.use('/', (req, res) => {
-    res.send('hello');
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    const tinystr = randomstring.generate(5);
+    res.send(`hello ${tinystr}`);
 });
 
 const PORT = process.env.PORT || 3232;
