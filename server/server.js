4f05978ceadc7fe6ec87afa245776a21c4bbf1d2
Gagandeep Singh <gagan.ddun@gmail.com>
2020-07-24 23:25:31 +1000

    code cleanup

--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const randomstring = require('randomstring');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
-
+const pass = 'f3!ER7N@GNs66w.';
+mongoose.connect( `mongodb://gagan:${encodeURIComponent(pass)}@ds353748.mlab.com:53748/tinyurl`, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection.on('open', () => {
+    console.log('connected');
+});
 app.get('/', (req, res) => {
     const tinystr = randomstring.generate(5);
     res.send(`hello ${tinystr}`);
